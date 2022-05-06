bb20d1435eb05b8a678ee904cf5d740929534278
sayeedofficial <sayeedofficial6220@gmail.com>
2022-05-06 23:39:49 +0530

    Directly Send Text File to Get Blink Count instead of CSV

--- server/blink.py
+++ server/blink.py
@@ -8,38 +8,43 @@ import plotly.graph_objects as go
 import re
 from scipy.signal import find_peaks
 
+
 filename = os.listdir("./files")[0]
 receivedFilepath = "./files/"+filename
-os.rename(receivedFilepath,"./files/data.csv")
+os.rename(receivedFilepath,"./files/data.txt")
+
+temp = pd.read_csv("./files/data.txt",delimiter="\t")
+headerList = ['Timestamp', 'Voltage']
+temp.to_csv("./files/data.csv",header= headerList,index=None);
 
 data = pd.read_csv("./files/data.csv")
 
-x = data['Time']
+x = data['Timestamp']
 y = data['Voltage']
 
-i = 0.0
-j = 1
-one_minute_time = []
-one_minute_voltage = []
+# i = 0.0
+# j = 1
+# one_minute_time = []
+# one_minute_voltage = []
 
-while i < 60.0:
-    one_minute_time.append(x[j])
-    one_minute_voltage.append(y[j])
-    i+=0.01
-    j+=1
+# while i < 60.0:
+#     one_minute_time.append(x[j])
+#     one_minute_voltage.append(y[j])
+#     i+=0.01
+#     j+=1
   
 fig = go.Figure(data=go.Scatter(
-    x = one_minute_time,
-    y = one_minute_voltage,
+    x = x,
+    y = y,
     mode = 'lines'
 ))
 
 #fig.show()
 
-voltage = one_minute_voltage
+voltage = y
 
 indices = find_peaks(voltage,threshold=0,distance = 50)[0]
-peaks = list(i for i in indices if voltage[i]>3.6 and voltage[i]<5)
+peaks = list(i for i in indices if voltage[i]>98)
 
 # fig = go.Figure()
 # fig.add_trace(go.Scatter(
@@ -61,4 +66,6 @@ peaks = list(i for i in indices if voltage[i]>3.6 and voltage[i]<5)
 #     name='Detected Peaks'
 # ))
 os.remove("./files/data.csv")
+os.remove("./files/data.txt")
+
 print(len(peaks))
