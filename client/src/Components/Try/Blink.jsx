4dd4b75a52383beee0297fa0b183c00179c79ea8
sayeedofficial <sayeedofficial6220@gmail.com>
2022-05-07 16:54:29 +0530

    Added Loader When a get blink button is called

--- client/src/Components/Try/Blink.jsx
+++ client/src/Components/Try/Blink.jsx
@@ -1,42 +1,55 @@
 import React from "react";
 import axios from "axios";
-
+import loading from "./loading.gif";
 import { Button } from "@mui/material";
+import "./Blink.css";
 class BlinkRate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-    this.getBlink = this.getBlink.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+		this.getBlink = this.getBlink.bind(this);
+		this.hitBlink = this.hitBlink.bind(this);
+	}
 
-  async getBlink() {
-    const cn = await axios.get("http://localhost:5500/blinkcount");
-    const data = cn.data;
-    console.log(cn);
-    this.setState({
-      count: data,
-    });
-  }
+	async getBlink() {
+		const cn = await axios.get("http://localhost:5500/blinkcount");
+		const data = cn.data;
+		this.setState({
+			count: data,
+		});
+	}
+	hitBlink() {
+		this.setState({ count: -1 });
+		this.getBlink();
+	}
 
-  render() {
-    return (
-      <div>
-        <h2> {this.state.count} </h2>
-        <br />
-        <Button
-          color="success"
-          size="small"
-          variant="contained"
-          onClick={this.getBlink}
-        >
-          {" "}
-          Get Blink Count
-        </Button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="blink-container">
+			
+				{this.state.count === -1 ? (
+					<img className="loading-img" width="25px" src={loading} alt=" " />
+				) : (
+					<h2 style={{ fontSize: "25px" }}> {this.state.count} </h2>
+				)}
+
+				<br />
+				<Button
+					color="success"
+					size="small"
+					variant="contained"
+					onClick={this.hitBlink}
+				>
+					{" "}
+					Get Blink Count
+				</Button>
+				<br />
+				<br />
+			</div>
+		);
+	}
 }
 
 export default BlinkRate;
--- client/src/Components/Try/Try.jsx
+++ client/src/Components/Try/Try.jsx
@@ -10,46 +10,47 @@ import "./Try.css";
 toast.configure();
 
 function Try() {
-  const [file, setFile] = useState();
-  const [fileName, setFileName] = useState("");
+	const [file, setFile] = useState();
+	const [fileName, setFileName] = useState("");
 
-  const saveFile = (e) => {
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0].name);
-  };
+	const saveFile = (e) => {
+		setFile(e.target.files[0]);
+		setFileName(e.target.files[0].name);
+	};
 
-  const uploadFile = async (e) => {
-    toast("File Uploaded Successfully");
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("fileName", fileName);
-    try {
-      const res = await axios.post("http://localhost:5500/upload", formData);
-      console.log(res);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
+	const uploadFile = async (e) => {
+		toast("File Uploaded Successfully");
+		const formData = new FormData();
+		formData.append("file", file);
+		formData.append("fileName", fileName);
+		try {
+			const res = await axios.post("http://localhost:5500/upload", formData);
+			console.log(res);
+		} catch (ex) {
+			console.log(ex);
+		}
+	};
 
-  return (
-    <div className="fileform-container">
-      <input type="file" onChange={saveFile} className="custom-file-input" />
+	return (
+		<div>
+			<div className="fileform-container">
+      <h4>Upload the Text File Containing Blink and Voltage Values </h4>
+				<input type="file" onChange={saveFile} className="custom-file-input" />
 
-      <Button
-        size="small"
-        startIcon={<SaveIcon />}
-        variant="contained"
-        onClick={uploadFile}
-      >
-        Upload
-      </Button>
-      <br />
-      <br />
-      <br />
-
-      <BlinkRate />
-    </div>
-  );
+				<Button
+					size="small"
+					startIcon={<SaveIcon />}
+					variant="contained"
+					onClick={uploadFile}
+				>
+					Upload
+				</Button>
+				
+				<br />
+			</div>
+			<BlinkRate />
+		</div>
+	);
 }
 
 export default Try;
